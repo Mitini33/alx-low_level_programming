9acc0e97947906c3d56c526f50d84f9b0d641251
mitini33 <ejirobready@gmail.com>
2022-09-09 09:15:29 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Printinlg in c
+ *
+ * Return: Always 0 (successful)
+ */
+int main(void)7
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

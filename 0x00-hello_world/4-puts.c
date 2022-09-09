2d7161f63d026d3df4124f787a0011b079adec21
mitini33 <ejirobready@gmail.com>
2022-09-09 10:28:11 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+*main - Entry point
+*vim 4-puts.c
+*Return: Always 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}

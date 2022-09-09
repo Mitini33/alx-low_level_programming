111fffdd489608c0c6629d1d8e74c4d7910fb170
mitini33 <ejirobready@gmail.com>
2022-09-09 10:14:40 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include <studio.h>
 
 /**
 *main - Entry point
 *
 *Return: Always 0
 */
-int main (void)
+int main(void)
 {
-printf(" with proper grammar, but the outcome is a piece of art, \n");
+puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
 }
